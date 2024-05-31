1f174ec260dc7a9ba0ef1d5dd7668bd2a546ba76
Ellis Osborn <ejosborn@asu.edu>
2024-05-31 11:14:28 -0700

    ranger creation unittest

--- tests.py
+++ tests.py
@@ -29,6 +29,28 @@ class TestCharacterCreation(unittest.TestCase):
             {"Gold": 50, "Potions": ["Healing Potion"], "Items": []},
         )
 
+    def test_ranger_character_creation(self):
+        """Test creating a ranger character.
+
+        This test checks if a Character object is created successfully
+        when creating a character of the 'ranger' class.
+
+        """
+        # Creating melee character
+        name = "Robin Hood"
+        _class_ = "ranger"
+        character = Character(name, _class_)
+
+        # Asserting the the character is created with correct attributes
+        self.assertEqual(character.name, name)
+        self.assertEqual(character._class_, _class_)
+        self.assertEqual(character.armor, "Common Leather Armor")
+        self.assertEqual(character.weapon, "Common Bow")
+        self.assertEqual(
+            character.inventory,
+            {"Gold": 50, "Potions": ["Healing Potion"], "Items": []},
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
