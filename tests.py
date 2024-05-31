899497f3007dc5539eae34dbf4b3d87ed5806155
Ellis Osborn <ejosborn@asu.edu>
2024-05-30 20:36:41 -0700

    basic unittests

---
+++ tests.py
@@ -0,0 +1,17 @@
+"""Unit tests for CLI adventures"""
+
+import unittest
+
+
+class TestCharacter(unittest.TestCase):
+    """
+    Tests for Character creation and values
+    """
+
+    def test_character_creation(self):
+        """Test that Character Creation is successful."""
+        print("Created Character")
+
+
+if __name__ == "__main__":
+    unittest.main()
